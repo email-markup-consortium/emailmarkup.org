529b8e8ba4fcf807fd013d643a1e38ec5d17daa4
Hussein Al Hammad <contact@hussein-alhammad.com>
2025-07-05 12:59:30 +0400

    RSS: include full article content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 
 export async function GET(context) {
@@ -16,8 +19,11 @@ export async function GET(context) {
       .map((post) => ({
         title: post.data.title,
         pubDate: post.data.pubDate,
-        description: post.data.description,
         link: `/blog/${post.slug}/`,
+        description: post.data.description,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        }),
       })),
   });
 }
